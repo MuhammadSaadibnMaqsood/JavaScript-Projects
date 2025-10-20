4db3138471acdefc0126aa61477f50d945dc89b0
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-10-20 06:29:08 -0700

    Complete About page and Product section on hero

--- E-commerce-With-Suapbase/script.js
+++ E-commerce-With-Suapbase/script.js
@@ -3,5 +3,8 @@ const nav_ul = document.getElementById("nav-ul");
 const lis = ["Home", "Products", "About"];
 
 lis.map((tag) => {
-  nav_ul.innerHTML += `<li class = "nav-item">${tag} <div class="line"></div> </li>  `;
+  nav_ul.innerHTML += `  <li class="nav-item">
+      <a href="${tag === "Home" ? "/" : tag + ".html"}">${tag}</a>
+      <div class="line"></div>
+    </li>`;
 });
