b58aba5b7ed582053f9827bbfb365150de2ccb38
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-10-20 10:59:19 -0700

    Working on Product Page

--- E-commerce-With-Suapbase/script.js
+++ E-commerce-With-Suapbase/script.js
@@ -8,3 +8,53 @@ lis.map((tag) => {
       <div class="line"></div>
     </li>`;
 });
+
+// HERO PRODUCTS
+
+const heroProducts = [
+  {
+    id: 1,
+    name: "Face Wash",
+    price: "500Rs",
+    img: "./assests/facewash.jpg",
+  },
+  {
+    id: 2,
+    name: "Glasses",
+    price: "600Rs",
+    img: "./assests/glasses.jpg",
+  },
+  {
+    id: 3,
+    name: "Head Phones",
+    price: "1500Rs",
+    img: "./assests/headphone.jpg",
+  },
+  {
+    id: 4,
+    name: "Watch",
+    price: "700Rs",
+    img: "./assests/watch.jpg",
+  },
+];
+
+const cardHolder = document.getElementById("card-holder");
+
+heroProducts.map((product) => {
+  cardHolder.innerHTML += `
+       <a href="Product.html?id=${product.id}">
+          <div class="card">
+            <div class="card-img">
+              <img src="${product.img}" alt="" />
+              <span>Best Seller</span>
+              <div class="hover-effect">
+                <div>
+                  <h1>${product.name}</h1>
+                  <p>Price: ${product.price}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+  `;
+});
